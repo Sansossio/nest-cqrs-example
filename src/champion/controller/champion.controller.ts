372377c36e5ae096fb250e44fb8ac4ae20cd315b
Sansossio <juliosansossio@gmail.com>
2020-06-02 08:33:46 +0200

    Simple implementation

--- src/champion/controller/champion.controller.ts
+++ src/champion/controller/champion.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
+import { LolApi } from 'twisted'
+import { Champions } from 'twisted/dist/constants'
+import { ApiResponse } from '@nestjs/swagger'
+import { ChampionDTO } from '../dto/champion.dto'
 
 @Controller('champion')
-export class ChampionController {}
+export class ChampionController {
+  @Get()
+  @ApiResponse({ type: ChampionDTO })
+  getChampion () {
+    return new LolApi().DataDragon.getChampion(Champions.YASUO)
+  }
+}
