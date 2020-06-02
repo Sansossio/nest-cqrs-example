cefa5fea397b8b48ff4746deda1ae4085e8ca0c5
Sansossio <juliosansossio@gmail.com>
2020-06-02 10:43:32 +0200

    Error handler

--- src/champion/queries/handlers/get-champion.handle.ts
+++ src/champion/queries/handlers/get-champion.handle.ts
@@ -3,6 +3,7 @@ import { GetChampionQuery } from '../impl/get-champion.query'
 import { LolApiService } from '../../../lol-api/service/lol-api.service'
 import { Champions } from 'twisted/dist/constants'
 import { ChampionDTO } from '../../dto/champion.dto'
+import { NotFoundException } from '@nestjs/common'
 
 @QueryHandler(GetChampionQuery)
 export class GetChampionHandler implements IQueryHandler<GetChampionQuery> {
@@ -12,6 +13,9 @@ export class GetChampionHandler implements IQueryHandler<GetChampionQuery> {
 
   async execute (query: GetChampionQuery): Promise<ChampionDTO> {
     const id = +Champions[query.champion]
+    if (!id) {
+      throw new NotFoundException()
+    }
     const response = await this.lolApi.dataDragon.getChampion(id)
     return {
       id,
